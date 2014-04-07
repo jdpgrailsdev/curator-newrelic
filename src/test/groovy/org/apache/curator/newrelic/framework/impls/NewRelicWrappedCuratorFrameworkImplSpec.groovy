a4ce20da26570cbfd1d6996fac65accd6922382c
Jonathan Pearlin <jpearlin1@gmail.com>
2014-04-07 09:49:29 -0400

    Additional unit tests.

--- src/test/groovy/org/apache/curator/newrelic/framework/impls/NewRelicWrappedCuratorFrameworkImplSpec.groovy
+++ src/test/groovy/org/apache/curator/newrelic/framework/impls/NewRelicWrappedCuratorFrameworkImplSpec.groovy
@@ -82,4 +82,256 @@ class NewRelicWrappedCuratorFrameworkImplSpec extends Specification {
         then:
             thrown IllegalArgumentException
     }
+
+    def "test that calls to start() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                start() >> {}
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.start()
+        then:
+            1 * delegate.start()
+    }
+
+    def "test that calls to close() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                close() >> {}
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.close()
+        then:
+            1 * delegate.close()
+    }
+
+    def "test that calls to getState() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getState() >> {}
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getState()
+        then:
+            1 * delegate.getState()
+    }
+
+    def "test that calls to isStarted() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                isStarted() >> { false }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.isStarted()
+        then:
+            1 * delegate.isStarted()
+    }
+
+    def "test that calls to create() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                create() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.create()
+        then:
+            1 * delegate.create()
+    }
+
+    def "test that calls to delete() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                delete() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.delete()
+        then:
+            1 * delegate.delete()
+    }
+
+    def "test that calls to checkExists() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                checkExists() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.checkExists()
+        then:
+            1 * delegate.checkExists()
+    }
+
+    def "test that calls to getData() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getData() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getData()
+        then:
+            1 * delegate.getData()
+    }
+
+    def "test that calls to setData() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                setData() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.setData()
+        then:
+            1 * delegate.setData()
+    }
+
+    def "test that calls to getChildren() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getChildren() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getChildren()
+        then:
+            1 * delegate.getChildren()
+    }
+
+    def "test that calls to getACL() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getACL() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getACL()
+        then:
+            1 * delegate.getACL()
+    }
+
+    def "test that calls to setACL() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                setACL() >> {  }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.setACL()
+        then:
+            1 * delegate.setACL()
+    }
+
+    def "test that calls to inTransaction() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                inTransaction() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.inTransaction()
+        then:
+            1 * delegate.inTransaction()
+    }
+
+    def "test that calls to sync(String,Object) invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                sync(_,_) >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.sync('path', new Object())
+        then:
+            1 * delegate.sync(_,_)
+    }
+
+    def "test that calls to sync() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                sync() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.sync()
+        then:
+            1 * delegate.sync()
+    }
+
+    def "test that calls to getConnectionStateListenable() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getConnectionStateListenable() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getConnectionStateListenable()
+        then:
+            1 * delegate.getConnectionStateListenable()
+    }
+
+    def "test that calls to getCuratorListenable() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getCuratorListenable() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getCuratorListenable()
+        then:
+            1 * delegate.getCuratorListenable()
+    }
+
+    def "test that calls to getUnhandledErrorListenable() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getUnhandledErrorListenable() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getUnhandledErrorListenable()
+        then:
+            1 * delegate.getUnhandledErrorListenable()
+    }
+
+    def "test that calls to nonNamespaceView() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                nonNamespaceView() >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.nonNamespaceView()
+        then:
+            1 * delegate.nonNamespaceView()
+    }
+
+    def "test that calls to usingNamespace(String) invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                usingNamespace(_) >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.usingNamespace('namespace')
+        then:
+            1 * delegate.usingNamespace(_)
+    }
+
+    def "test that calls to getNamespace() invoke the underlying delegate"() {
+        setup:
+            CuratorFramework delegate = Mock() {
+                getNamespace(_) >> { null }
+            }
+            def client = new NewRelicWrappedCuratorFrameworkImpl(delegate)
+        when:
+            client.getNamespace()
+        then:
+            1 * delegate.getNamespace()
+    }
 }
